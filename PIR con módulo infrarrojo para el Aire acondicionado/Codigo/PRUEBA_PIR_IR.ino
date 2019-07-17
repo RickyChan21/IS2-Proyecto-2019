1770d1ac5fdcd6523bd3f7eb33140bb5dae9dad9
HugoBinns <52224327+HugoBinns@users.noreply.github.com>
2019-07-17 09:34:24 -0500

    Update PRUEBA_PIR_IR.ino

--- PIR con módulo infrarrojo para el Aire acondicionado/Codigo/PRUEBA_PIR_IR.ino
+++ PIR con módulo infrarrojo para el Aire acondicionado/Codigo/PRUEBA_PIR_IR.ino
@@ -1,10 +1,11 @@
-#include <Wire.h>
-#include <ESP8266WiFi.h>
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"
-#include <Arduino.h>
-#include <IRremoteESP8266.h>
-#include <IRsend.h>
+    
+#include <Wire.h>// Libreria Para usar puertos SDA SSL
+#include <ESP8266WiFi.h>//Libreria Para utilizar El wifi en Node MCU
+#include <Adafruit_MQTT.h>//Librería para conexion del protocolo MQTT
+#include <Adafruit_MQTT_Client.h>//Librería para conexion del protocolo MQTT Cliente
+#include <Arduino.h>//Librería que importa los protocolos de arduino al NodeMCU
+#include <IRremoteESP8266.h>//Libreria para el módulo infrarrojo del NodeMCU
+#include <IRsend.h>//Libre para enviar señal infrarroja a través del módulo infrarrojo
 
 //Estas son las credenciales del wifi al cuál el NodeMCU se va a aconectar
 #define WLAN_SSID       "SkynetMobile"
@@ -18,76 +19,76 @@
 #define USERNAME    "username"
 #define PASSWORD    "12345678" 
 
-const int intervall = 10000;
+const int intervall = 10000; //Intervalo de tiempo para la conexión al core
 
 
-WiFiClient client;
-Adafruit_MQTT_Client mqtt(&client, HOST, PORT, USERNAME, PASSWORD);
+WiFiClient client; //Suscripción del NodeMCU como cliente
+Adafruit_MQTT_Client mqtt(&client, HOST, PORT, USERNAME, PASSWORD); //Función que realiza la conexión entre el NodeMCU y el core
 
 
-Adafruit_MQTT_Publish estado1 = Adafruit_MQTT_Publish(&mqtt, "estado1");
+Adafruit_MQTT_Publish estado1 = Adafruit_MQTT_Publish(&mqtt, "estado1"); //Función que publica los valores obtenidos con el NodeMCU
 
-void MQTT_connect();
+void MQTT_connect(); //Método que hace la conexión entre el NodeMCU y el core
 
 //Definición de Variables
-const uint16_t kIrLed = 4;  
-const int PIR = 14;
-const int LED = 16;
-int estado;
-int pirState = LOW;
-IRsend irsend(kIrLed);
+const uint16_t kIrLed = 4;  //Pin dónde está conectado el módulo infrarrojo
+const int PIR = 14; //Pin dónde está conectado el PIR
+const int LED = 16; //Pin dónde está conectado el LED
+int estado; //Variable que obtendrá el estado del pir
+int pirState = LOW; //Variable auxiliar del estado del pir
+IRsend irsend(kIrLed); //Declaramos la variable kIrLed como emisor de señal infrarroja
 
 void setup() {
-  irsend.begin();
+  irsend.begin(); //iniciamos el módulo infrarrojo
   //Definicion de las variables de salida y entrada
-  pinMode(PIR, INPUT);
-  pinMode(LED, OUTPUT);
+  pinMode(PIR, INPUT); //Declaramos el PIR como entrada
+  pinMode(LED, OUTPUT); //Declaramos el LED como salida
 
-  WiFi.forceSleepWake();
-  WiFi.mode(WIFI_STA);
-  Serial.begin(115200);
-  Serial.print("Connecting to ");
-  Serial.println(WLAN_SSID);
+  WiFi.forceSleepWake(); //Despierta el módulo Wifi
+  WiFi.mode(WIFI_STA); //Inicializa el módulo wifi
+  Serial.begin(115200); //Esta es la frecuencia en la que trabaja el NodeMCU
+  Serial.print("Connecting to "); //Mensaje de conexión
+  Serial.println(WLAN_SSID); //Impresión del SSID del wifi al que nos estamos conectando
 
-  WiFi.begin(WLAN_SSID, WLAN_PASS);
-  while (WiFi.status() != WL_CONNECTED) 
+  WiFi.begin(WLAN_SSID, WLAN_PASS); //Inicia la conección del NodeMCU al wifi con sus credenciales
+  while (WiFi.status() != WL_CONNECTED)  //Mientras el Wifi no esté conectado imprime un punto a cada 5 milisegundos
   {
     delay(500);
     Serial.print(".");
   }
   
   Serial.println();
-  Serial.println("WiFi connected");
-  Serial.println("IP address: "); Serial.println(WiFi.localIP());
-  MQTT_connect();
+  Serial.println("WiFi connected"); //Imprime que el wifi está conectado
+  Serial.println("IP address: "); Serial.println(WiFi.localIP()); //Imprime el IP
+  MQTT_connect(); //Conexión con el protocolo MQTT
 }
 
 void loop() {
-  estado = digitalRead(PIR);
+  estado = digitalRead(PIR); //Se lee el estado del PIR
   //Si el estado del PIR es activo, se enciende el AIRE acondicionado
   if (estado == HIGH)
   {
-    digitalWrite(LED, HIGH);
-    if (pirState == LOW)
+    digitalWrite(LED, HIGH); //Se enciende el LED
+    if (pirState == LOW) //Si el PIR está apagado
     {
-      irsend.sendSony(0xa90, 12);
-      Serial.println("Encendido");
-      pirState = HIGH;
-      estado1.publish("Encendido");
-      delay(60000);
+      irsend.sendSony(0xa90, 12); //Se envía la señal infrarroja de encendido
+      Serial.println("Encendido"); //Se imprime "encendido
+      pirState = HIGH; //La variable auxiliar cambia a HIGH
+      estado1.publish("Encendido"); //Se publica "Encendido" en el core
+      delay(60000); //Retraso de 1 minuto
     }
   }
   //Si el estado del PIR es inactivo, se apaga el AIRE acondicionado
   else
   {
-    digitalWrite(LED, LOW);
+    digitalWrite(LED, LOW); //Se apaga el LED
     if (pirState == HIGH)
     {
-      irsend.sendSony(0xa90, 12);
-      Serial.println("Apagado");
-      pirState = LOW;
-      estado1.publish("Apagado");
-      delay(10000);
+      irsend.sendSony(0xa90, 12); //Se envía la señal de apagado
+      Serial.println("Apagado"); //Se imprime que se apagó
+      pirState = LOW; //La variable auxiliar cambia a LOW
+      estado1.publish("Apagado"); //Se publica "Apagado" en el core
+      delay(10000); //Retraso de 10 segundos
     }
   }
   
